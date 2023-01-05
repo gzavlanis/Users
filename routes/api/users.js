73477dc97f5500409409772e2f704e77bdd6174f
gzavlanis <gzavlanis@gmail.com>
2023-01-05 17:47:53 +0200

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,43 +3,40 @@ const router = express.Router();
 const redis= require('redis');
 
 // create connection to Redis
-client= redis.createClient();
-
-client.on("error", function (err) {
-    console.log("Error: " + err);
-});
-
-client.on("connect", () => {
-	console.log("Connected with Redis successfully.");
+const client= redis.createClient({ legacyMode: true});
+client.connect().then(async (res) => {
+	console.log('Redis connected');
+}).catch((err) => {
+	console.log('There was an error: ' + err);
 });
 
 // show all users
 router.get('/', (req, res) => {
-	client.hgetall('User:');
+	res.json(client.keys('*'));
 });
 
 // show specific user
 router.get('/:id', (req, res) => {
 	id = parseInt(req.params.id);
-	res.json(client.hget('User' + `${id}`));
+	res.json(client.hmGet('User' + `${id}`, 'first_name', 'last_name', 'email'));
 });
 
 // create a new user
+var id;
 router.post('/', (req, res) => {
 	const { first_name, last_name, email } = req.body;
-	var id= 0;
 
 	if (!first_name || !last_name || !email) {
 		return res.status(400).send('You have empty fields! Try again.');
+	} else {
+		client.hSet('User' + `${id.toString()}`, 
+			'first_name', `${first_name}`,
+			'last_name', `${last_name}`,
+			'email', `${email}`
+		);
+		res.send('User created successfully!');
+		id++;
 	}
-
-	id++;
-	client.hmset('User' + `${parseInt(id)}`, {
-		'first_name': `${first_name}`,
-		'last_name': `${last_name}`,
-		'email': `${email}`
-	});
-	
 });
 
 // update a user
@@ -50,18 +47,20 @@ router.put('/:id', (req, res) => {
 	if (!first_name || !last_name || !email) {
 		return res.status(400).send('You have empty fields! Try again.');
 	} else {
-		client.hmset('User' + `${id}`, {
-			'first_name': `${first_name}`,
-			'last_name': `${last_name}`,
-			'email': `${email}`
-		});
+		client.hSet('User' + `${id}`,
+			'first_name', `${first_name}`,
+			'last_name', `${last_name}`,
+			'email', `${email}`
+		);
+		res.send('User updated successfully!');
 	}
 });
 
 // delete user
 router.delete('/:id', (req, res) => {
 	id = parseInt(req.params.id);
-	client.hdel('User' + `${id}`);
+	client.del('User' + `${id}`);
+	res.send('User deleted.');
 });
 
 module.exports = router;
